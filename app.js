6c5263ad81e0bbba8329d33fb5975ff23f37ba99
Joel Brown <joel.anthony.brown1@gmail.com>
2021-01-26 16:06:04 -0500

    feat: setting up server adding in logging for any requests adding in template engine and configuring it for the server

--- app.js
+++ app.js
@@ -2,12 +2,31 @@
 const express = require("express");
 // bring in the environmental varialbes
 const dotenv = require("dotenv");
+// morgan logs information when requesting a route or page 
+const morgan = require('morgan');
+// template engine
+const exphbs = require('express-handlebars');
+// connect to the database
+const connectDB = require('./config/db');
 
 // load config
 dotenv.config({path: './config/config.env'});
 
+// connect to the db
+connectDB();
+
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// middleware for the server:
+if (process.env.NODE_ENV === 'develop') {
+  app.use(morgan('dev'));
+}
+// Handlebars
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`server started in ${process.env.NODE_ENV}, on port ${PORT}`));
