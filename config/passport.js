03fd341a0601b9bf7b2a8adf3c33322f14108d32
Joel Brown <joel.anthony.brown1@gmail.com>
2021-02-01 13:53:34 -0500

    feat: adding users to db

--- config/passport.js
+++ config/passport.js
@@ -11,9 +11,33 @@ module.exports = function(passport) {
     callbackURL: '/auth/google/callback' // path to callback - needs to match exactly as found in app.js and routes/auth.js
   }, 
   async (accessToken, refreshToken, profile, done) => {
-    console.log('do we get to the google strategy async function');
-    console.log(profile) 
+    console.log(profile);
+
+    // build a new person profile
+    const newPerson = {
+      googleId: profile.id,
+      displayName: profile.displayName,
+      firstName: profile.name.givenName,
+      lastName: profile.name.familyName,
+      image: profile.photos[0].value
+    }
+
+    // check to see if the person is already in the database
+    try {
+      let person = await Person.findOne({ googleId: profile.id})
+      console.log('do we have a person: ', person);
+      if ( person === null ) {
+        console.log('we should be adding a person to the db');
+        // if person doesn't exist in the database, create a new record for the person
+        person = await Person.create(newPerson);
+      }
+      // return the person without an error (null is space holder for error)
+      done(null, person);
+    } catch (err) {
+      console.error('we had an error trying to create a new person in the DB: ', err)
+    }
     // 'done' is called to signify that we are finished with all the logic and ready to continue
+
   }));
 
   // more explanation given in passport docs for the use of serialize and deserialize functions:
